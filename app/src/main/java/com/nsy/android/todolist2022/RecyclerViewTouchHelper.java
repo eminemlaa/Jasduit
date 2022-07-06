7afae9a166bb26687676c43ebc3dedadeda58ad7
syahidahyunos <nursyahidahyunos@gmail.com>
2022-07-07 01:35:46 +0800

    updated UI

--- app/src/main/java/com/nsy/android/todolist2022/RecyclerViewTouchHelper.java
+++ app/src/main/java/com/nsy/android/todolist2022/RecyclerViewTouchHelper.java
@@ -58,7 +58,7 @@ public class RecyclerViewTouchHelper extends ItemTouchHelper.SimpleCallback {
     @Override
     public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
         new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
-                .addSwipeLeftBackgroundColor(ContextCompat.getColor(adapter.getContext() , R.color.colorPrimaryDark))
+                .addSwipeLeftBackgroundColor(ContextCompat.getColor(adapter.getContext() , R.color.colorGreen))
                 .addSwipeLeftActionIcon(R.drawable.ic_baseline_edit)
                 .addSwipeRightBackgroundColor(Color.RED)
                 .addSwipeRightActionIcon(R.drawable.ic_baseline_delete)
